e0441b36bcd91a0a2acf09d8a5e587e242d65d57
Lovelish Chauhan <83005550+lovelish-xd@users.noreply.github.com>
2025-01-26 15:15:03 +0530

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    // await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect("mongodb+srv://luciferispro69:Lucifer%4069@cluster0.jdzvl.mongodb.net/portfolioTracker", { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
